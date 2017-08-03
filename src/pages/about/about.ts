caac8884121f209cdeaa0b118873b438e6c2e767
tianlu <1074025714@qq.com>
2017-08-03 17:55:57 +0800

    ionic add welcome page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {WelcomePage} from "../welcome/welcome";
+
 
 @Component({
   selector: 'page-about',
@@ -10,5 +12,7 @@ export class AboutPage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  showWelcome(){
+    this.navCtrl.push(WelcomePage);
+  }
 }
