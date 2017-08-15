8011ad5a3f9a5dc6606c7cc2ff33587bdbc6d225
tianlu <1074025714@qq.com>
2017-08-15 18:01:30 +0800

    后台修改

--- node_manage/test_mysql.js
+++ node_manage/test_mysql.js
@@ -3,10 +3,11 @@
  */
 
 var mysql  = require('mysql');  //调用MySQL模块
-var express = require('express')();
+var express = require('express');
+var app = express();
 
-
-express.all('*', function(req, res, next) {
+/*设置响应头*/
+app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
@@ -15,58 +16,64 @@ express.all('*', function(req, res, next) {
     next();
 });
 
-//配置服务端口
-var server = express.listen(3000, function () {
+//配置api接口端口
+var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
-})
+});
 
-//创建一个connection
-var connection = mysql.createConnection({
+
+
+//写个register api
+app.get('/register',function(req,res){
+  //创建一个connection 配置数据库信息
+  var connection = mysql.createConnection({
     host     : 'localhost',       //主机
     user     : 'root',            //MySQL认证用户名
     password : 'ionic123',        //MySQL认证用户密码
     port: '3306',                 //端口号
     database:'ionic-login'        //库名
-});
-//创建一个connection
-connection.connect(function(err){
-    if(err){
-        console.log('[query] - :'+err);
-        return;
-    }
-    console.log('[connection connect]  succeed!');
-});
-
-
-//写个接口register
-express.get('/register',function(req,res){
-  //执行SQL语句 插入数据
-  connection.query('select * from userinfo where username=tianlu', function(err, rows, fields) {
-    console.log(111);
-    if (err) {
-        console.log('[query] - :'+err);
-        return;
-      }
-
-    if(req.query.username == rows[0].username){//procedure
-      console.log(222);
-      res.status(200),
-          res.json("存在此人");
-      }else{
-      console.log(333);
-      res.status(200),
-          res.send("输入的不对给我重新输入");
-      }
-     console.log(333);
   });
 
+  //connection 开始连接数据库
+    connection.connect(function(err){
+      if(err){
+        console.log('[connection start]:'+err);
+        return;
+      }
+      console.log('[connection start]: open mysql succeed!');
+    });
+
+
+    //执行SQL语句 插入数据
+    connection.query("select * from userinfo where username='"+req.query.username+"'", function(err, rows, fields) {
+
+      if (err) {//查询失败
+        console.log('[query] - :'+err);
+        return;
+      }
+
+      if(rows[0]== undefined){
+        res.status(200),
+        res.send("该账号可以使用");
+      }else if(req.query.username == rows[0].username){//procedure
+        res.status(200),
+        res.send("此账号已存在，请重新输入");
+      }
+
+      //关闭connection
+      connection.end(function(err){
+        if(err){
+          console.log('[connection end]:'+err);
+          return;
+        }
+        console.log('[connection end]close mysql succeed!');
+      });
+
+
+    });
+
+
 });
 
-//关闭connection
-connection.end(function(err){
-    if(err){
-        return;
-    }
-    console.log('[connection end] succeed!');
-});
+
