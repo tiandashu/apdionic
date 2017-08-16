3982fc88ce59037c6e63e3e3bf58c0be77093db8
tianlu <1074025714@qq.com>
2017-08-16 13:28:13 +0800

    修复bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
-import { Splashscreen } from '@ionic-native/splash-screen';
+import { SplashScreen } from '@ionic-native/splash-screen';
 import { Storage} from '@ionic/storage'
 
-// import { Splashscreen } from 'ionic-native';
 
 
 import {WelcomePage} from "../pages/welcome/welcome";
 import {RegisterPage} from "../pages/user/register/register";
-import {LoginPage} from "../pages/user/login/login";
+// import {LoginPage} from "../pages/user/login/login";
 
 
 @Component({
@@ -19,7 +18,7 @@ export class MyApp {
 
   rootPage:any;// = WelcomePage
 
-  constructor(platform: Platform, statusBar: StatusBar, splashscreen: Splashscreen,private storage:Storage) {
+  constructor(platform: Platform, statusBar: StatusBar, splashscreen: SplashScreen,private storage:Storage) {
     /*通过storage的缓存信息判断是不展示欢迎页*/
     this.storage.get('firstIn').then((result)=>{
 
@@ -37,8 +36,8 @@ export class MyApp {
       statusBar.styleDefault();
       // splashScreen.hide();
       setTimeout(function () {
-        Splashscreen.hide();
-        }, 1000);
+        splashscreen.hide();
+      }, 1000);
 
     });
   }
